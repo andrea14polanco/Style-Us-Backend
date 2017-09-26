c34a0b2f58949aa98276c4aad92fac3b299ca3f9
migueloc19 <migueloviedoc@gmail.com>
2017-09-26 00:00:25 -0700

    Se inicializo el hastmap

--- src/main/java/com/styleus/backend/controller/Application.java
+++ src/main/java/com/styleus/backend/controller/Application.java
@@ -1,4 +1,16 @@
+
 package com.styleus.backend.controller;
 
+import com.styleus.backend.models.User;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+
 public class Application {
+
+    public static HashMap <Long, User> hmUser = new HashMap<Long, User>();
+
+
 }
