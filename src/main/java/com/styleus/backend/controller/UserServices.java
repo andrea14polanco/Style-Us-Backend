ef5819e98ac27e02d39a2e2e411e31bd00ea09e7
Adeury Camilo <adeury09@gmail.com>
2017-09-28 23:37:51 -0400

    Agregando servidor con el methodo login listo sin errores pero el registro sin fecha de nacimiento

--- src/main/java/com/styleus/backend/controller/UserServices.java
+++ src/main/java/com/styleus/backend/controller/UserServices.java
@@ -1,5 +1,6 @@
 package com.styleus.backend.controller;
 
+import com.styleus.backend.StyleUsBackendMain;
 import com.styleus.backend.models.User;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,21 +19,22 @@ public class UserServices {
 
     @RequestMapping(value="/",method = RequestMethod.GET)
     public HashMap<Long,User> getAllUsers(){
-        return Application.hmUser;
+        return StyleUsBackendMain.hmUser;
     }
 
 
     @RequestMapping(value="/registration",method = RequestMethod.POST)
     public User userRegistration(@RequestParam(value="name") String name,
                                  @RequestParam(value = "first_surname") String first_surname,
-                                 @RequestParam(value = "date_of_birth") Date date_of_birth,
+                                 //@RequestParam(value = "date_of_birth") Date date_of_birth,
                                  @RequestParam(value = "gender") Boolean gender,
                                  @RequestParam(value = "email") String email,
                                  @RequestParam(value = "password") String password,
                                  @RequestParam(value = "privacity") Boolean privacity){
 
-        User usuario = new User(name,first_surname,date_of_birth,true,email,password,privacity);
-        Application.hmUser.put(new Long(usuario.getId()),usuario);
+        User usuario = new User(name,first_surname,/*date_of_birth,*/gender,email,password,privacity);
+        StyleUsBackendMain.hmUser.put(new Long(usuario.getId()),usuario);
+        //Application.hmUser.put(new Long(usuario.getId()),usuario);
         return usuario;
     }
 
@@ -41,9 +43,9 @@ public class UserServices {
                           @RequestParam(value = "password") String password){
         boolean encontrado = false;
         Integer contador =0;
-        ;
 
-        for (Map.Entry<Long, User> temporal: Application.hmUser.entrySet()){
+
+        for (Map.Entry<Long, User> temporal: StyleUsBackendMain.hmUser.entrySet()){
                  if(temporal.getValue().getEmail().equals(email) && temporal.getValue().getPassword().equals(password)) {
                      encontrado = true;
                      break;
