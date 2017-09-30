19bd37f8d1a7c5fe3b7944b6df77a7aa0ba040ce
migueloc19 <migueloviedoc@gmail.com>
2017-09-30 00:41:41 -0700

    Se crea modelo formal usuario para StyleUs

--- src/main/java/com/styleus/backend/models/User.java
+++ src/main/java/com/styleus/backend/models/User.java
@@ -1,4 +1,173 @@
 package com.styleus.backend.models;
 
+import java.util.Date;
+
 public class User {
+
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private int gender;
+    private String name;
+    private String middle_name;
+    private String first_surname;
+    private String second_surname;
+    private String complete_name;
+    private Date date_of_birth;
+    private String profile_img_url;
+    private int privacity;
+    private int follower_count;
+    private Date created_at;
+    private Date updated_at;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMiddle_name() {
+        return middle_name;
+    }
+
+    public void setMiddle_name(String middle_name) {
+        this.middle_name = middle_name;
+    }
+
+    public String getFirst_surname() {
+        return first_surname;
+    }
+
+    public void setFirst_surname(String first_surname) {
+        this.first_surname = first_surname;
+    }
+
+    public String getSecond_surname() {
+        return second_surname;
+    }
+
+    public void setSecond_surname(String second_surname) {
+        this.second_surname = second_surname;
+    }
+
+    public String getComplete_name() {
+        return complete_name;
+    }
+
+    public void setComplete_name(String complete_name) {
+        this.complete_name = complete_name;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public String getProfile_img_url() {
+        return profile_img_url;
+    }
+
+    public void setProfile_img_url(String profile_img_url) {
+        this.profile_img_url = profile_img_url;
+    }
+
+    public int getPrivacity() {
+        return privacity;
+    }
+
+    public void setPrivacity(int privacity) {
+        this.privacity = privacity;
+    }
+
+    public int getFollower_count() {
+        return follower_count;
+    }
+
+    public void setFollower_count(int follower_count) {
+        this.follower_count = follower_count;
+    }
+
+    public Date getCreated_at() {
+        return created_at;
+    }
+
+    public void setCreated_at(Date created_at) {
+        this.created_at = created_at;
+    }
+
+    public Date getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(Date updated_at) {
+        this.updated_at = updated_at;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", gender=" + gender +
+                ", name='" + name + '\'' +
+                ", middle_name='" + middle_name + '\'' +
+                ", first_surname='" + first_surname + '\'' +
+                ", second_surname='" + second_surname + '\'' +
+                ", complete_name='" + complete_name + '\'' +
+                ", date_of_birth=" + date_of_birth +
+                ", profile_img_url='" + profile_img_url + '\'' +
+                ", privacity=" + privacity +
+                ", follower_count=" + follower_count +
+                ", created_at=" + created_at +
+                ", updated_at=" + updated_at +
+                '}';
+    }
 }
