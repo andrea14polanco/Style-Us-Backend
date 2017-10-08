f87a7ac32167fd19d157027598a3c8c52293f852
Adeury Camilo <adeury09@gmail.com>
2017-10-08 16:21:11 -0400

    Cambios en los usuarios que se crean automaticamente en el main

--- src/main/java/com/styleus/backend/StyleUsBackendMain.java
+++ src/main/java/com/styleus/backend/StyleUsBackendMain.java
@@ -1,5 +1,6 @@
 package com.styleus.backend;
 
+//import com.styleus.backend.models.Post;
 import com.styleus.backend.models.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,19 +14,25 @@ import java.util.HashMap;
 public class StyleUsBackendMain  extends SpringBootServletInitializer{
 
     public static HashMap<Long, User> hmUser;
+    //public static HashMap<Long, Post> hmPost;
 
     public static void main(String[] args) {
 
         hmUser = new HashMap<Long, User>();
+        //hmPost = new HashMap<Long, Post>();
+
 
         SimpleDateFormat sdf = new SimpleDateFormat();
         //Date dt = sdf.parse("10/10/200");
 
-        User uno = new User("miguel1","Oviedo",true, "adeury@gmail.com","12345",false);
-        hmUser.put(new Long(uno.getId()),uno);
+        User uno = new User((long) 1,"miguel1","Oviedo",true, "adeury@gmail.com","12345",false);
+        hmUser.put(uno.getId(),uno);
 
-        User dos = new User("dos","Oviedo",true, "adeury@gmail.com","12345",false);
-        hmUser.put(new Long(dos.getId()),dos);
+        User dos = new User((long)2,"dos","Oviedo",true, "adeury@gmail.com","12345",false);
+        hmUser.put(dos.getId(),dos);
+
+        //Post post1 = new Post(1,"Este es el cuerpo del post",5,0,"C:\\Users\\Adeury\\IdeaProjects\\Style-Us-Backend\\img\\imagen1",uno.getId(),1,1);
+        //hmPost.put(post1.getId(),post1);
 
         SpringApplication.run(StyleUsBackendMain.class, args);
     }
