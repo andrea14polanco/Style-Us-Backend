b2411f11f42dbea8b11738c94fa916e991a034e7
migueloc19 <migueloviedoc@gmail.com>
2017-10-12 00:28:29 -0700

    Agregando haspMap para guardar los action state

--- src/main/java/com/styleus/backend/StyleUsBackendMain.java
+++ src/main/java/com/styleus/backend/StyleUsBackendMain.java
@@ -1,9 +1,6 @@
 package com.styleus.backend;
 
-import com.styleus.backend.models.Comments;
-import com.styleus.backend.models.Post;
-import com.styleus.backend.models.Like;
-import com.styleus.backend.models.User;
+import com.styleus.backend.models.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
@@ -19,6 +16,7 @@ public class StyleUsBackendMain  extends SpringBootServletInitializer{
     public static HashMap<Long, Post> hmPost;
     public static HashMap<Long, Like> hmLike;
     public static HashMap<Long, Comments> hmComments;
+    public static HashMap<Long, ActionStates> hmActionStates;
 
     public static void main(String[] args) {
 
@@ -26,6 +24,8 @@ public class StyleUsBackendMain  extends SpringBootServletInitializer{
         hmPost = new HashMap<Long, Post>();
         hmLike = new HashMap<Long, Like>();
         hmComments = new HashMap<Long, Comments>();
+        hmActionStates = new HashMap<Long, ActionStates>();
+
 
 
         SimpleDateFormat sdf = new SimpleDateFormat();
@@ -39,6 +39,11 @@ public class StyleUsBackendMain  extends SpringBootServletInitializer{
 
         Post post1 = new Post(1,"Este es el cuerpo del post",5,0,"C:\\Users\\Adeury\\IdeaProjects\\Style-Us-Backend\\img\\imagen1",uno.getId(),1,1);
         hmPost.put(post1.getId(),post1);
+
+        ActionStates accion1 = new ActionStates((long) 1,"like"," le a dado like a tu publicacion");
+        ActionStates accion2 = new ActionStates((long) 2,"comentario"," a comentado tu publicacion");
+        hmActionStates.put(accion1.getId(),accion1);
+        hmActionStates.put(accion2.getId(),accion2);
 
         SpringApplication.run(StyleUsBackendMain.class, args);
     }
