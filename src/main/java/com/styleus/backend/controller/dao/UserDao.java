7805c17f54df6e8ebedf2510726cc54cb8cb7618
migueloc19 <migueloviedoc@gmail.com>
2017-10-15 17:24:58 -0700

    Eliminando carpeta dao

--- src/main/java/com/styleus/backend/controller/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-//package com.styleus.backend.controller.dao;
-//
-//import com.styleus.backend.models.User;
-//import org.springframework.data.repository.CrudRepository;
-//
-//public interface UserDao extends CrudRepository <User,Long>{
-//    boolean exists(String email);
-//}
