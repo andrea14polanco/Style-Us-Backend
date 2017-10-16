3b566a9524162ed8bbee442e7335aef16be34ff1
Andrea <alpm9615@gmail.com>
2017-10-16 01:38:58 -0400

    Se agregan primeros named queries al modelo

--- src/main/java/com/styleus/backend/models/User.java
+++ src/main/java/com/styleus/backend/models/User.java
@@ -1,31 +1,108 @@
 package com.styleus.backend.models;
 
+
+import javax.persistence.*;
 import java.util.Date;
+@Entity
+@Table(schema = "dbo",name = "Users")
+@NamedQueries({
+        @NamedQuery(name = "User.findByUsername", query = "select u from User u where u.username = ?1"),
+        @NamedQuery(name = "User.findByEmail", query = "select u from User u where u.email = ?1")
+})
 
 public class User {
 
-    private int id;
-    private String username;
-    private String password;
-    private String email;
-    private int gender;
-    private String name;
-    private String middle_name;
-    private String first_surname;
-    private String second_surname;
-    private String complete_name;
-    private Date date_of_birth;
-    private String profile_img_url;
-    private int privacity;
-    private int follower_count;
-    private Date created_at;
-    private Date updated_at;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
 
-    public int getId() {
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+      @Column(name = "email")
+    private String email;
+
+    @Column(name = "gender")
+    private int gender;
+
+   @Column(name = "name")
+    private String name;
+
+  @Column(name = "middle_name")
+    private String middle_name;
+
+  @Column(name = "first_surname")
+    private String first_surname;
+
+   @Column(name = "second_surname")
+    private String second_surname;
+
+    @Column(name = "complete_name")
+    private String complete_name;
+
+    @Column(name = "date_of_birth")
+    private Date date_of_birth;
+
+    @Column(name = "profile_img_url")
+    private String profile_img_url;
+
+    @Column(name = "privacity")
+    private int privacity;
+
+    @Column(name = "follower_count")
+    private Long follower_count;
+
+    @Transient
+    private Date created_at;
+
+    @Column(name ="update_at")
+    private Date update_at;
+
+    public User() {
+    }
+
+    public User(String username, String password, String email, int gender, String name, String middle_name, String first_surname, String second_surname, String complete_name, Date date_of_birth, String profile_img_url, int privacity, Long follower_count, Date update_at) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.gender = gender;
+        this.name = name;
+        this.middle_name = middle_name;
+        this.first_surname = first_surname;
+        this.second_surname = second_surname;
+        this.complete_name = complete_name;
+        this.date_of_birth = date_of_birth;
+        this.profile_img_url = profile_img_url;
+        this.privacity = privacity;
+        this.follower_count = follower_count;
+        this.update_at = update_at;
+    }
+    public User(User user) {
+        this.username = user.username;
+        this.password = user.password;
+        this.email = user.email;
+        this.gender = user.gender;
+        this.name = user.name;
+        this.middle_name = user.middle_name;
+        this.first_surname = user.first_surname;
+        this.second_surname = user.second_surname;
+        this.complete_name = user.complete_name;
+        this.date_of_birth = user.date_of_birth;
+        this.profile_img_url = user.profile_img_url;
+        this.privacity = user.privacity;
+        this.follower_count = user.follower_count;
+        System.out.println(new Date());
+        this.update_at =  new Date();
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
@@ -125,11 +202,11 @@ public class User {
         this.privacity = privacity;
     }
 
-    public int getFollower_count() {
+    public Long getFollower_count() {
         return follower_count;
     }
 
-    public void setFollower_count(int follower_count) {
+    public void setFollower_count(Long follower_count) {
         this.follower_count = follower_count;
     }
 
@@ -142,11 +219,11 @@ public class User {
     }
 
     public Date getUpdated_at() {
-        return updated_at;
+        return update_at;
     }
 
     public void setUpdated_at(Date updated_at) {
-        this.updated_at = updated_at;
+        this.update_at = updated_at;
     }
 
     @Override
@@ -167,7 +244,7 @@ public class User {
                 ", privacity=" + privacity +
                 ", follower_count=" + follower_count +
                 ", created_at=" + created_at +
-                ", updated_at=" + updated_at +
+                ", updated_at=" + update_at +
                 '}';
     }
 }
